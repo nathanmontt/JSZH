491076d07b78c303e2c1a1d12c3ccd624c8c9d91
nathanmontt <contato.mont@gmail.com>
2025-03-26 22:00:56 -0300

    Adição das outras configs. para o dark/light mode

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,24 +5,34 @@ const btnCtaDarkTheme = document.getElementById('dark-theme');
 
 const home = document.querySelector('.home-link');
 const info = document.querySelector('.info-footer-link');
+const desc = document.querySelector('.desc');
 
 btnCtaDarkTheme.addEventListener('click', function() {
     if (body.classList.contains('light-theme')) {
         body.classList.remove('light-theme');
         body.classList.add('dark-theme');
 
-        btnCtaDarkTheme.textContent = 'LIGHT';
-        btnCtaDarkTheme.style.color = '#f3f3f3';
-        home.style.color = '#f3f3f3';
-        info.style.color = '#f3f3f3';
+        changeColorDarkMode();
     } else {
         body.classList.remove('dark-theme')
         body.classList.add('light-theme')
-
-        btnCtaDarkTheme.textContent = 'DARK';
-        btnCtaDarkTheme.style.color = '#373734';
-        home.style.color = '#373734';
-        info.style.color = '#373734';
+        
+        changeColorWhiteMode();
     }
-    // console.log('asdadasd')
 });
+
+function changeColorDarkMode () {
+    btnCtaDarkTheme.textContent = 'LIGHT';
+    btnCtaDarkTheme.style.color = '#f3f3f3';
+    home.style.color = '#f3f3f3';
+    info.style.color = '#f3f3f3';
+    desc.style.color = '#f3f3f3';
+}
+
+function changeColorWhiteMode () {
+    btnCtaDarkTheme.textContent = 'DARK';
+    btnCtaDarkTheme.style.color = '#373734';
+    home.style.color = '#373734';
+    info.style.color = '#373734';
+    desc.style.color = '#373734';
+}
