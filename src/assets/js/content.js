daddb09e61c783849a3772cd603fcf61881555fb
nathanmontt <contato.mont@gmail.com>
2025-03-31 18:08:20 -0300

    Testes

---
+++ src/assets/js/content.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let i;
+const containerStudyExplanation = document.querySelectorAll('.container-text-explanations');
+
+for (i=0 ; i<containerStudyExplanation.length ; i++) {
+    containerStudyExplanation[i].classList.add('mt-1')
+}
