25466097fdc8e6ad101ddea29613168250b35ccd
nathanmontt <contato.mont@gmail.com>
2025-04-01 09:11:19 -0300

    Testes
    Testando a possibilidade de criar um objeto que armazene todo o conteúdo para fácil acesso e escrita dos estudos

--- src/assets/js/content.js
+++ src/assets/js/content.js
@@ -12,14 +12,43 @@
 //     el.classList.add('mb-1');
 // })
 
-let i;
+const contentDesc = [
+    {
+        titleClass: `inicio das aulas`,
+        content: [
+            `teste <br>`,
+            `teste2 <br>`,
+            `teste2 <br>`,
+            `teste2 <br>`,
+            `teste2 <br>`,
+            `teste2 <br>`,
+            `teste3 <br>`,
+            `teste3 <br>`,
+            `teste3 <br>`,
+            `teste3 <br>`,
+            `teste3 <br>`,
+            `teste4 <br>`,
+            `teste4 <br>`,
+            `teste4 <br>`,
+            `teste4 <br>`,
+            `teste4 <br>`,
+        ]
+    }
+]
+
+// container do html que vai guardar o conteúdo
 const divContainer = document.querySelectorAll('.container-explanation-text');
 
+// for para adicionar em "todos os elemento possiveis"
 for (let i=0 ; i<divContainer.length; i++) {
     // divContainer[i]
     const parag = document.createElement('p');
+    const title = document.createElement('h4');
     parag.classList.add('explanation-text');
-    parag.innerHTML = "lorem ipsum"
+    title.classList.add('title-explanation-text');
 
-    divContainer[i].append(parag)
+    title.innerHTML = contentDesc[i].titleClass;
+    parag.innerHTML = contentDesc[i].content;
+
+    divContainer[i].append(title, parag)
 }
