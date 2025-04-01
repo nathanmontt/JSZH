645ab4af06e63cebf56a7398b0f105230a8b2263
nathanmontt <contato.mont@gmail.com>
2025-03-31 22:22:44 -0300

    Testes

--- src/assets/js/content.js
+++ src/assets/js/content.js
@@ -1,8 +1,13 @@
 'use strict'
 
 let i;
-const containerStudyExplanation = document.querySelectorAll('.container-text-explanations');
+const containerStudyExplanation = document.querySelectorAll('.container-explanation-text');
+const elTextExplanation = document.querySelectorAll('.explanation-text');
 
 for (i=0 ; i<containerStudyExplanation.length ; i++) {
     containerStudyExplanation[i].classList.add('mt-1')
 }
+
+elTextExplanation.forEach(el => {
+    el.classList.add('mb-1');
+})
