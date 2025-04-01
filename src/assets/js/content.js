63cc8097bb0f4a2f240d5870c609033b6ade1546
nathanmontt <contato.mont@gmail.com>
2025-04-01 08:58:42 -0300

    Testes

--- src/assets/js/content.js
+++ src/assets/js/content.js
@@ -1,13 +1,25 @@
 'use strict'
 
+// let i;
+// const containerStudyExplanation = document.querySelectorAll('.container-explanation-text');
+// const elTextExplanation = document.querySelectorAll('.explanation-text');
+
+// for (i=0 ; i<containerStudyExplanation.length ; i++) {
+//     containerStudyExplanation[i].classList.add('mt-1')
+// }
+
+// elTextExplanation.forEach(el => {
+//     el.classList.add('mb-1');
+// })
+
 let i;
-const containerStudyExplanation = document.querySelectorAll('.container-explanation-text');
-const elTextExplanation = document.querySelectorAll('.explanation-text');
+const divContainer = document.querySelectorAll('.container-explanation-text');
 
-for (i=0 ; i<containerStudyExplanation.length ; i++) {
-    containerStudyExplanation[i].classList.add('mt-1')
+for (let i=0 ; i<divContainer.length; i++) {
+    // divContainer[i]
+    const parag = document.createElement('p');
+    parag.classList.add('explanation-text');
+    parag.innerHTML = "lorem ipsum"
+
+    divContainer[i].append(parag)
 }
-
-elTextExplanation.forEach(el => {
-    el.classList.add('mb-1');
-})
